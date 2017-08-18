9903782d5ff10c48e390e02923acb3f0f477456f
radlee <lolito@projectcodex.co>
2017-08-18 09:41:18 +0200

    Experimental Web App Commit 1

---
+++ app.py
@@ -0,0 +1,328 @@
+from flask import Flask, render_template, request, flash, redirect, url_for, session, logging
+# from data import Stories #Import Stories to get Access to returned dumy data
+from flaskext.mysql import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+#Import pymysql for cursor to return a dictionary for selecting
+#stories from the database
+import pymysql
+import csv
+import os
+import tablib
+#Import fucntools for checking if users are looged in
+from functools import wraps
+app = Flask(__name__)
+
+#Configure MySQL
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
+app.config['MYSQL_DATABASE_DB'] = 'Stories'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+#Initialize MySQL
+mysql = MySQL(app)
+
+#Variable  and equate it to the Storie's function returning stories data
+# Stories = Stories()
+
+dataset = tablib.Dataset()
+with open(os.path.join(os.path.dirname(__file__), 'static/files/Week1.csv')) as f :
+    dataset.csv = f.read()
+
+    data = dataset.csv
+
+    res = data.splitlines()
+
+    listOfLists = []
+    listOfObjects = []
+    for item in res:
+        res = item.split(",")
+        listOfLists.append(res)
+
+        for item in listOfLists:
+            for i in item:
+                d = {}
+                # d["Day"] = item[0]
+                d["Date"] = item[1]
+                d["Product"] = item[2]
+                d["Number_Sold"] = item[3]
+                d["Price"] = item[4]
+            listOfObjects.append(d)
+            print listOfObjects
+
+
+    # print listOfObjects
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/week')
+def readFile():
+    # print dataset
+    # with open('/static/files/week1.csv', 'r') as inFile:
+        # reader = csv.reader(inFile)
+        # linesList = [row for row in reader]
+    return render_template('week.html', dataset=dataset)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/stories')
+def stories():
+
+    #Create cursor that return Dictionary
+    conn = mysql.connect()
+    cur = conn.cursor(pymysql.cursors.DictCursor)
+
+    #Get Stories
+    result = cur.execute("SELECT * FROM stories")
+    #This must be in a Dictionary form -- json
+    stories = cur.fetchall()
+
+    print stories
+
+    if result > 0:
+        return render_template('stories.html',stories=stories )
+    else:
+        msg = 'No Stories Found'
+        return render_template('stories.html', msg=msg)
+    #Close connection
+    cur.close() #data
+
+#Single Story
+@app.route('/story/<string:id>/') # id = Dynamic Value
+def story(id):
+    #Create cursor that return Dictionary
+    conn = mysql.connect()
+    cur = conn.cursor(pymysql.cursors.DictCursor)
+
+    #Get Stories
+    result = cur.execute("SELECT * FROM stories WHERE id = %s", [id])
+    #This must be in a Dictionary form -- json
+    story = cur.fetchone()
+
+    return render_template('story.html', story=story) #data
+
+
+#WTForms ----------------
+class RegisterForm(Form):
+    name = StringField("Name", [validators.Length(min=1, max=50)])
+    username = StringField("Username", [validators.Length(min=4, max=25)])
+    email = StringField("Email", [validators.Length(min=6, max=50)])
+    password = PasswordField("Password", [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords Do Not Match')
+    ])
+    confirm = PasswordField('Confirm Password')
+# Register--------------
+#Accept POST requests---
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    #form variable
+    form = RegisterForm(request.form)
+    #Check what type of request ...POST or GET
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        #Encrypt Password before storing it
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        #Create the cursor
+        conn = mysql.connect()
+        cur = conn.cursor()
+
+        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
+
+        #Commit to DB
+        conn.commit()
+        #Close connection
+        cur.close()
+
+        flash('You are now registered and can log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+#Login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        #Get username and Passwords from the form--
+        username = request.form['username']
+        password_candidate = request.form['password']
+
+        #Create a cursor
+        conn = mysql.connect()
+        cur = conn.cursor()
+
+        #Get user by username
+        result = cur.execute("SELECT * FROM users WHERE username = %s", [username])
+
+        if result > 0:
+            #Get the stored hash
+            data = cur.fetchone()
+            password = data[4]
+
+            #Compare Passwords
+            if sha256_crypt.verify(password_candidate, password):
+                print  "MATCHED"
+                #Passed
+                session['logged_in'] = True
+                session['username'] = username
+
+                flash("Login successfull", 'success')
+                return redirect(url_for('dashboard'))
+
+            else:
+                error = "Invalid login"
+                return render_template('login.html',error=error)
+            #Close Connection
+            cur.close()
+        else:
+            error = "Username not found"
+
+    return render_template('login.html')
+
+#Check if the User is logged in
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Login to view this page', 'danger')
+            return redirect(url_for('login'))
+    return wrap
+
+#Logout
+@app.route('/logout')
+@is_logged_in
+def logout():
+    session.clear()
+    flash("Logged Out", 'success')
+    return redirect(url_for('login'))
+
+
+#Dashboard
+@app.route('/dashboard')
+@is_logged_in
+def dashboard():
+    #Create cursor that return Dictionary
+    conn = mysql.connect()
+    cur = conn.cursor(pymysql.cursors.DictCursor)
+
+    #Get Stories
+    result = cur.execute("SELECT * FROM stories")
+    #This must be in a Dictionary form -- json
+    stories = cur.fetchall()
+
+    print stories
+
+    if result > 0:
+        return render_template('dashboard.html',stories=stories )
+    else:
+        msg = 'No Stories Found'
+        return render_template('dashboard.html', msg=msg)
+    #Close connection
+    cur.close()
+
+class StoryForm(Form):
+    title = StringField('Title', [validators.Length(min=1, max=200)])
+    body = TextAreaField('Body', [validators.Length(min=30)])
+
+#Add Story-----
+@app.route('/add_story', methods=['GET', 'POST'])
+@is_logged_in
+def add_story():
+    form = StoryForm(request.form)
+    if request.method == 'POST' and form.validate():
+        title = form.title.data
+        body = form.body.data
+
+        # Create a cursor
+        conn = mysql.connect()
+        cur = conn.cursor()
+
+        # Execute
+        cur.execute("INSERT INTO stories(title, body, author) VALUES(%s, %s, %s)", (title, body, session['username']))
+
+        #Commit
+        conn.commit()
+
+        #Close connection
+        cur.close()
+        flash('Stroy Created', 'success')
+        return redirect(url_for('dashboard'))
+
+    return render_template('add_story.html', form=form)
+
+# Edit Story
+@app.route('/edit_story/<string:id>', methods=['GET', 'POST'])
+@is_logged_in
+def edit_story(id):
+    #Fill the form
+    #Create cursor that return Dictionary
+    conn = mysql.connect()
+    cur = conn.cursor(pymysql.cursors.DictCursor)
+
+    # Get the story by id
+
+    result = cur.execute("SELECT * FROM stories WHERE id = %s", [id])
+
+    story = cur.fetchone()
+
+    #Get the form
+    form = StoryForm(request.form)
+
+    #Populate story form fields
+    form.title.data = story['title']
+    form.body.data = story['body']
+
+    if request.method == 'POST' and form.validate():
+
+        title = request.form['title']
+        body = request.form['body']
+
+        #Create cursor that return Dictionary
+        conn = mysql.connect()
+        cur = conn.cursor(pymysql.cursors.DictCursor)
+
+        # Execute
+        cur.execute("UPDATE stories SET title=%s,body=%s WHERE id = %s", (title, body, id))
+
+        #Commit
+        conn.commit()
+
+        #Close connection
+        cur.close()
+        flash('Story Updated', 'success')
+        return redirect(url_for('dashboard'))
+
+    return render_template('edit_story.html', form=form)
+
+#Delete Story
+@app.route('/delete_story/<string:id>', methods=["POST"])
+@is_logged_in
+def delete_story(id):
+    #Create cursor that return Dictionary
+    conn = mysql.connect()
+    cur = conn.cursor(pymysql.cursors.DictCursor)
+
+    #Execute
+    cur.execute("DELETE FROM stories WHERE id = %s", [id])
+    #Commit
+    conn.commit()
+
+    #Close connection
+    cur.close()
+    flash('Story Deleted', 'success')
+    return redirect(url_for('dashboard'))
+
+
+if __name__ == '__main__':
+    app.secret_key = 'secret123'
+    app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,40 @@
+def Stories():
+    stories = [
+        {
+            'id' : 1,
+            'title' : 'How to Build a Blog like an Architect',
+            'body' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'authour' : 'Leander',
+            'create_date' : '21 August 2017'
+        },
+        {
+            'id' : 2,
+            'title' : 'Sunrise Sunshine - A movie about a girl nextdoor',
+            'body' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'authour' : 'Maxwell',
+            'create_date' : '22 August 2017'
+        },
+        {
+            'id' : 3,
+            'title' : 'Tender Poetry',
+            'body' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'authour' : 'Blake',
+            'create_date' : '23 August 2017'
+        },
+        {
+            'id' : 4,
+            'title' : 'Internet : Tutorial Series - Omnibus',
+            'body' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'authour' : 'Nightblood',
+            'create_date' : '24 August 2017'
+        },
+        {
+            'id' : 5,
+            'title' : 'We both know I am the Joker - Stand Up Comedy Deslates',
+            'body' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'authour' : 'Nightblood',
+            'create_date' : '25 August 2017'
+        }
+    ]
+
+    return stories
